6faee5a4c80ffd1f5780562c13055b0d29badd0e
Kathan Cheema <kathancheema@outlook.com>
2020-02-25 18:54:13 +0000

    Rubiks cube renders corners.

--- rubiks-cube-solver-web/src/App.tsx
+++ rubiks-cube-solver-web/src/App.tsx
@@ -1,27 +1,15 @@
-import * as React from 'react';
-import './App.css';
-
-const logo = require("./logo.svg") as string;
+import React from 'react';
+import './resources/App.css';
+import RubiksGame from "./app/rubiksGame";
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    onload = () => {
+
+    };
+
+    return (
+        <RubiksGame/>
+    );
 }
 
 export default App;
